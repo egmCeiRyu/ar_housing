7d2e5ec5ab5e76caffb09a45ef0a94c14f243d0a
egmCeiRyu <egami@ceiryu.com>
2026-06-25 12:47:52 +0900

    Update admin-login.js

--- js/admin-login.js
+++ js/admin-login.js
@@ -60,15 +60,6 @@ async function login(){
         data.id
     );
 
-    sessionStorage.setItem("admin","true");
-
-    console.log(
-        sessionStorage.getItem("admin")
-    );
-
-    window.location.href =
-    `${BASE_PATH}/index.html`;
-
     window.location.href =
     `${BASE_PATH}/index.html`;
 
