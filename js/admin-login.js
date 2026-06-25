62db9cff590d7103b35cc89a3a5aa0bfc77e1f86
egmCeiRyu <egami@ceiryu.com>
2026-06-25 13:54:17 +0900

    Update admin-login.js

--- js/admin-login.js
+++ js/admin-login.js
@@ -60,14 +60,7 @@ sessionStorage.setItem(
     data.id
 );
 
-console.log("admin =", sessionStorage.getItem("admin"));
-console.log("admin_id =", sessionStorage.getItem("admin_id"));
-console.log("redirect =", `${BASE_PATH}/index.html`);
-
-// Pare aqui para depuração
-debugger;
-
-// Só continua quando você clicar Resume (F8) no DevTools
-window.location.href = `${BASE_PATH}/index.html`;
+window.location.href =
+`${BASE_PATH}/index.html`;
 
 }
